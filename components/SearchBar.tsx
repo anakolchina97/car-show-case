2230a5cbccbed88065d71330c6f0fc4841730925
Annie <93747286+anakolchina97@users.noreply.github.com>
2023-12-13 18:58:23 +0300

    SearchBar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,9 +3,23 @@
 import { useState } from "react";
 
 import { SearchManufacturer } from ".";
+import Image from "next/image";
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button className={`-ml-3 z-10 ${otherClasses}`} type="submit">
+    <Image
+      src="/magnifying-glass.svg"
+      alt="magnifying-glass"
+      width={40}
+      height={40}
+      className="object-contain"
+    />
+  </button>
+);
 
 const SearchBar = () => {
   const [manufacturer, setManuFacturer] = useState("");
+  const [model, setModel] = useState("");
 
   const handleSearch = () => {};
 
@@ -16,7 +30,27 @@ const SearchBar = () => {
           manufacturer={manufacturer}
           setManuFacturer={setManuFacturer}
         />
+        <SearchButton otherClasses="sm:hidden" />
       </div>
+      <div className="searchbar__item">
+        <Image
+          className="absolute w-[20px] h-[20px] ml-4"
+          src="/model-icon.png"
+          width={25}
+          height={25}
+          alt="car model"
+        />
+        <input
+          className="searchbar__input"
+          type="text"
+          name="model"
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder="Tiguan"
+        />
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+      <SearchButton otherClasses="max-sm:hidden" />
     </form>
   );
 };
